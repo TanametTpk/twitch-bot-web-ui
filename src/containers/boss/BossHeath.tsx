6af4786fdc09863a369e306e79701a0afd660c7e
tanamettpk <tanamet.tpk@gmail.com>
2021-06-09 20:12:28 +0700

    + boss heath

--- src/containers/boss/BossHeath.tsx
+++ src/containers/boss/BossHeath.tsx
@@ -1,14 +1,30 @@
+import { Progress } from 'antd'
 import React from 'react'
 import boss from './assets/fire-boss.svg'
 
 interface Props {
-    
+
 }
 
 const BossHeath = (props: Props) => {
     return (
-        <div>
-            {boss}
+        <div
+            style={{
+                width: '25vw'
+            }}
+        >
+            <img
+                style={{
+                    width: '100%'
+                }}
+                src={boss}
+            />
+            <Progress
+                percent={70}
+                size="small"
+                status="exception"
+                showInfo={false}
+            />
         </div>
     )
 }
